832bcd55aa853a1ad43c65e769040924f468b69d
gizm00 <sev@thedatascout.com>
2016-06-07 15:57:32 -0400

    removing extraneous code

--- __init__.py
+++ __init__.py
@@ -99,43 +99,12 @@ def getFacilityNames():
 
 		items_list=[];
 		for item in data:
-			items_list.append({'FacilityName':item[0]})
+			items_list.append({'facilityname':item[0]})
 		return {'StatusCode':'200','Items':items_list}
 
 	except Exception as ex:
 		return {'error':str(ex)}
 
-class Welcome(Resource):
-	@set_renderers(HTMLRenderer)
-	def get(self) :
-		return render_template('index.html')
-
-class GetFacilityNames(Resource):
-	def get(self):
-		try :
-			cursor = mysql.connection.cursor()
-				#cursor = conn.cursor()
-			cursor.callproc('sp_GetFacilityNames')
-			data = cursor.fetchall()
-
-			items_list=[];
-			for item in data:
-				i = {
-					'Name':item[0]
-				}
-				items_list.append(i)
-			return {'StatusCode':'200','Items':items_list}
-
-		except Exception as ex:
-			return {'error':str(ex)}
-
-class HelloWorld(Resource):
-	def get(self):
-		return {'hello': 'world'}
-
-#api.add_resource(Welcome, '/')
-#api.add_resource(GetFacilityNames, '/GetFacilityNames')
-#api.add_resource(Welcome, '/')		
 
 if __name__ == "__main__":
 	app.run()
