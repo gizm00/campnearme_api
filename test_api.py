0871fe784a613924a22b54ffe2ff99269183c2be
Sev Leonard <jennifer.m.leonard@gmail.com>
2016-06-07 17:02:11 -0700

    updating datadef formatting

--- test_api.py
+++ test_api.py
@@ -4,7 +4,7 @@ from pandas.io.json import json_normalize
 import requests
 
 
-response = requests.get(url="http://api.campnear.me/GetFacilityNames")
+response = requests.get(url="http://api.campnear.me/GetAllFacilities")
 try :
 	data = json.loads(response.text)
 	df = json_normalize(data['Items'])
@@ -14,4 +14,4 @@ except Exception as ex:
 	print("RidbData.extract(): unable to read response")
 	print(ex)
 
-df.to_csv('test.csv', index=False)
+df.to_csv('test.csv', index=False, encoding="utf-8")
